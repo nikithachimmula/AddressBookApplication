9949fdecf837a7283e0e57df6739f3b6200bda5b
nikitha chimmmula <nikithachimmula97@gmail.com>
2022-08-01 11:59:29 +0530

    Added: COMMON_ENVIRONAMENT

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -2,12 +2,15 @@ package com.bridgelabz.addressbookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookAppApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(AddressBookAppApplication.class, args);
+        ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+        log.info("Address Book Application Started in {} Environment", context.getEnvironment().getProperty("environment"));
     }
-
 }
